593249275a5623b9f13b1cc235b0dbeb5c733840
Simon Lindgren <simon.g.lindgren@gmail.com>
2021-10-19 00:26:16 +0200

    Added tests

---
+++ webapp/src/modules/home/Home.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Home } from './Home';
+
+test('renders home page', () => {
+  render(<Home />);
+  const title = screen.getByRole('heading', { name: /^tactics trainer$/i });
+  expect(title).toBeInTheDocument();
+  const button = screen.getByRole('button', { name: /^get random puzzle$/i });
+});
