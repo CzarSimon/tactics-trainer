ec95afad6b9c747ba1bcb1ea4bc9e45d2abfb7da
Simon Lindgren <simon.g.lindgren@gmail.com>
2021-10-25 14:41:34 +0300

    iam-service: added FindByUsername method on UserRepository

--- iam-service/internal/repository/user_repository.go
+++ iam-service/internal/repository/user_repository.go
@@ -11,6 +11,7 @@ import (
 
 type UserRepository interface {
 	Save(ctx context.Context, user models.User) error
+	FindByUsername(ctx context.Context, username string) (models.User, bool, error)
 }
 
 func NewUserRepository(db *sql.DB) UserRepository {
@@ -25,18 +26,59 @@ type userRepo struct {
 
 const saveUserQuery = `
 	INSERT INTO user_account(id, username, role, password, salt, data_encryption_key, key_encryption_key_id, created_at, updated_at)
-	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-`
+	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
 
 func (r *userRepo) Save(ctx context.Context, user models.User) error {
 	span, ctx := opentracing.StartSpanFromContext(ctx, "user_repo_save")
 	defer span.Finish()
 
 	dek := user.DataEncryptionKey
-	_, err := r.db.Exec(saveUserQuery, user.ID, user.Username, user.Role, user.Password, user.Salt, dek.Body, dek.KeyEncryptionKeyID, user.CreatedAt, user.UpdatedAt)
+	_, err := r.db.ExecContext(ctx, saveUserQuery, user.ID, user.Username, user.Role, user.Password, user.Salt, dek.Body, dek.KeyEncryptionKeyID, user.CreatedAt, user.UpdatedAt)
 	if err != nil {
 		return fmt.Errorf("failed to save %s: %w", user, err)
 	}
 
 	return nil
+}
+
+const findUserByUsernameQuery = `
+	SELECT 
+		id, 
+		username, 
+		role, 
+		password, 
+		salt, 
+		data_encryption_key, 
+		key_encryption_key_id, 
+		created_at, 
+		updated_at
+	FROM 
+		user_account
+	WHERE
+		username = ?`
+
+func (r *userRepo) FindByUsername(ctx context.Context, username string) (models.User, bool, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "user_repo_find_by_username")
+	defer span.Finish()
+
+	var u models.User
+	err := r.db.QueryRowContext(ctx, findUserByUsernameQuery, username).Scan(
+		&u.ID,
+		&u.Username,
+		&u.Role,
+		&u.Password,
+		&u.Salt,
+		&u.DataEncryptionKey.Body,
+		&u.DataEncryptionKey.KeyEncryptionKeyID,
+		&u.CreatedAt,
+		&u.UpdatedAt,
+	)
+	if err == sql.ErrNoRows {
+		return models.User{}, false, nil
+	}
+	if err != nil {
+		return models.User{}, false, fmt.Errorf("failed to query for user with username = %s. Error: %w", username, err)
+	}
+
+	return u, true, nil
 }
--- iam-service/internal/repository/user_repository_test.go
+++ iam-service/internal/repository/user_repository_test.go
@@ -3,6 +3,7 @@ package repository_test
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"testing"
 
 	"github.com/CzarSimon/httputil/id"
@@ -44,4 +45,51 @@ func Test_userRepo_Save(t *testing.T) {
 	err = db.QueryRow("SELECT id FROM user_account WHERE id = ?", user.ID).Scan(&foundID)
 	assert.NoError(err)
 	assert.Equal(user.ID, foundID)
+
+	ctx, cancel := context.WithCancel(ctx)
+	cancel()
+
+	err = repo.Save(ctx, user)
+	assert.Error(err)
+	assert.True(errors.Is(err, context.Canceled))
+}
+
+func Test_userRepo_FindByUsername(t *testing.T) {
+	assert := assert.New(t)
+	db := testutil.InMemoryDB(true, "../../resources/db/sqlite")
+	repo := repository.NewUserRepository(db)
+	ctx := context.Background()
+
+	user := models.User{
+		ID:       id.New(),
+		Username: "test-user",
+		Role:     models.UserRole,
+		Password: "test-password",
+		Salt:     "some-salt",
+		DataEncryptionKey: models.DataEncryptionKey{
+			Body:               "hex-encoded-string",
+			KeyEncryptionKeyID: 0,
+		},
+		CreatedAt: timeutil.Now(),
+		UpdatedAt: timeutil.Now(),
+	}
+
+	_, found, err := repo.FindByUsername(ctx, user.Username)
+	assert.NoError(err)
+	assert.False(found)
+
+	err = repo.Save(ctx, user)
+	assert.NoError(err)
+
+	foundUser, found, err := repo.FindByUsername(ctx, user.Username)
+	assert.NoError(err)
+	assert.True(found)
+	assert.Equal(user, foundUser)
+
+	ctx, cancel := context.WithCancel(ctx)
+	cancel()
+
+	_, _, err = repo.FindByUsername(ctx, user.Username)
+	assert.Error(err)
+	assert.True(errors.Is(err, context.Canceled))
 }
