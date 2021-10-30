3a06cc3d725234cb3e83b086e7f30a43c42ece88
Simon Lindgren <simon.g.lindgren@gmail.com>
2021-10-31 02:18:12 +0300

    puzzle-server: switched prod db type to mysql

--- puzzle-server/cmd/server/main.go
+++ puzzle-server/cmd/server/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/CzarSimon/tactics-trainer/puzzle-server/internal/api"
 	"github.com/CzarSimon/tactics-trainer/puzzle-server/internal/config"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
