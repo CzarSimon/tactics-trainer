73fd073e4e46b59c3f61a8fe65f3a05db73406ec
Simon Lindgren <simon.g.lindgren@gmail.com>
2021-11-07 11:02:21 +0100

    webapp: created problme set modal

---
+++ webapp/src/modules/problemsets/components/ProblemSetModal.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Modal } from 'antd';
+import { useProblemSet } from '../../../hooks';
+
+interface Props {
+  id: string;
+  onClose: () => void;
+}
+
+export function ProblemSetModal({ id, onClose }: Props) {
+  const problemSet = useProblemSet(id);
+  const [open, setOpen] = useState(true);
+  if (!problemSet) {
+    return null;
+  }
+
+  const close = () => {
+    setOpen(false);
+    onClose();
+  };
+
+  const { name, description, ratingInterval, puzzleIds, createdAt } = problemSet;
+  return (
+    <Modal visible={open} onCancel={close} footer={null}>
+      <h1>{name}</h1>
+      <p>{description}</p>
+      <p>
+        <b>Rating interval: </b>
+        {ratingInterval}
+      </p>
+      <p>
+        <b>Problems: </b>
+        {puzzleIds.length}
+      </p>
+      <p>
+        <b>Created: </b>
+        {createdAt}
+      </p>
+    </Modal>
+  );
+}
--- webapp/src/modules/problemsets/components/ProblemSetsList.tsx
+++ webapp/src/modules/problemsets/components/ProblemSetsList.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Spin } from 'antd';
 import { Optional, ProblemSet } from '../../../types';
 import { ProblemSetCard } from './ProblemSetCard';
 
 import styles from './ProblemSetsList.module.css';
+import { ProblemSetModal } from './ProblemSetModal';
 
 interface Props {
   problemSets: Optional<ProblemSet[]>;
   select: (id: string) => void;
 }
 
-export function ProblemSetsList({ problemSets, select }: Props) {
+export function ProblemSetsList({ problemSets }: Props) {
+  const [selectedId, setSelectedId] = useState<Optional<string>>(undefined);
+  const selectProblemSet = (id: string) => {
+    setSelectedId(id);
+  };
+  const unselectProblemSet = () => {
+    setSelectedId(undefined);
+  };
+
   return (
     <div className={styles.ProblemSetsList}>
       <h1>Problem Sets</h1>
       <div className={styles.ListContent}>
         {!problemSets && <Spin size="large" />}
-        {problemSets && problemSets.map((s) => <ProblemSetCard key={s.id} problemSet={s} select={select} />)}
+        {problemSets && problemSets.map((s) => <ProblemSetCard key={s.id} problemSet={s} select={selectProblemSet} />)}
       </div>
+      {selectedId && <ProblemSetModal id={selectedId} onClose={unselectProblemSet} />}
     </div>
   );
 }
