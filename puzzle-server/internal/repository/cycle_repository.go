11f672f598c30b493d9aaf834663117303482f30
Simon Lindgren <simon.g.lindgren@gmail.com>
2021-11-10 20:46:20 +0100

    puzzle-server: FindByProblemSetID method to CycleRepository

--- puzzle-server/internal/repository/cycle_repository.go
+++ puzzle-server/internal/repository/cycle_repository.go
@@ -14,6 +14,7 @@ import (
 type CycleRepository interface {
 	Save(ctx context.Context, c models.Cycle) error
 	Find(ctx context.Context, id string) (models.Cycle, bool, error)
+	FindByProblemSetID(ctx context.Context, problemSetId string, onlyActive bool) ([]models.Cycle, error)
 }
 
 func NewCycleRepository(db *sql.DB) CycleRepository {
@@ -82,4 +83,61 @@ func (r *cycleRepo) Find(ctx context.Context, id string) (models.Cycle, bool, er
 
 	c.CompleatedAt = compleatedAt.Time
 	return c, true, nil
+}
+
+const findCycleByProblemIDQuery = `
+	SELECT 
+		id, 
+		number, 
+		problem_set_id, 
+		current_puzzle_id, 
+		compleated_at, 
+		created_at, 
+		updated_at
+	FROM
+		cycle
+	WHERE 
+		problem_set_id = ?`
+
+func (r *cycleRepo) FindByProblemSetID(ctx context.Context, problemSetId string, onlyActive bool) ([]models.Cycle, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "cycle_repo_find_by_problem_set_id")
+	defer span.Finish()
+
+	query := createFindCycleByProblemSetIDQuery(onlyActive)
+	rows, err := r.db.QueryContext(ctx, query, problemSetId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query cycle by problem_set_id=%s: %w", problemSetId, err)
+	}
+	defer rows.Close()
+
+	cycles := make([]models.Cycle, 0)
+	var c models.Cycle
+	var compleatedAt sql.NullTime
+	for rows.Next() {
+		err = rows.Scan(
+			&c.ID,
+			&c.Number,
+			&c.ProblemSetID,
+			&c.CurrentPuzzleID,
+			&compleatedAt,
+			&c.CreatedAt,
+			&c.UpdatedAt,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan cycle row. Error: %w, problem_set_id=%s", err, problemSetId)
+		}
+		c.CompleatedAt = compleatedAt.Time
+		cycles = append(cycles, c)
+	}
+
+	return cycles, nil
+}
+
+func createFindCycleByProblemSetIDQuery(onlyActive bool) string {
+	condition := ""
+	if onlyActive {
+		condition = " AND compleated_at IS NULL"
+	}
+
+	return findCycleByProblemIDQuery + condition
 }
--- puzzle-server/internal/repository/cycle_repository_test.go
+++ puzzle-server/internal/repository/cycle_repository_test.go
@@ -134,3 +134,79 @@ func Test_cycleRepo_Find(t *testing.T) {
 	_, _, err = cycleRepo.Find(ctx, cycle.ID)
 	assert.True(errors.Is(err, context.Canceled))
 }
+
+func Test_cycleRepo_FindByProblemSetID(t *testing.T) {
+	assert := assert.New(t)
+	db := testutil.InMemoryDB(true, "../../resources/db/sqlite")
+	seedPuzzles(t, db)
+	psRepo := repository.NewProblemSetRepository(db)
+	cycleRepo := repository.NewCycleRepository(db)
+	ctx := context.Background()
+
+	set := models.ProblemSet{
+		ID:             id.New(),
+		Name:           "ps-name",
+		Themes:         []string{"passedPawn", "endgame"},
+		RatingInterval: "1300 - 1500",
+		CreatedAt:      timeutil.Now(),
+		UpdatedAt:      timeutil.Now(),
+		UserID:         "user-0",
+		PuzzleIDs: []string{
+			"puzzle-0",
+			"puzzle-1",
+			"puzzle-2",
+		},
+	}
+
+	err := psRepo.Save(ctx, set)
+	assert.NoError(err)
+
+	cycle1 := models.Cycle{
+		ID:              id.New(),
+		Number:          1,
+		ProblemSetID:    set.ID,
+		CurrentPuzzleID: "puzzle-2",
+		CompleatedAt:    timeutil.Now(),
+		CreatedAt:       timeutil.Now(),
+		UpdatedAt:       timeutil.Now(),
+	}
+	cycle2 := models.Cycle{
+		ID:              id.New(),
+		Number:          2,
+		ProblemSetID:    set.ID,
+		CurrentPuzzleID: "puzzle-0",
+		CreatedAt:       timeutil.Now(),
+		UpdatedAt:       timeutil.Now(),
+	}
+
+	cycles, err := cycleRepo.FindByProblemSetID(ctx, set.ID, false)
+	assert.NoError(err)
+	assert.NotNil(cycles)
+	assert.Empty(cycles)
+
+	err = cycleRepo.Save(ctx, cycle1)
+	assert.NoError(err)
+	err = cycleRepo.Save(ctx, cycle2)
+	assert.NoError(err)
+
+	cycles, err = cycleRepo.FindByProblemSetID(ctx, set.ID, false)
+	assert.NoError(err)
+	assert.Len(cycles, 2)
+	assert.Equal(cycle1, cycles[0])
+	assert.Equal(cycle2, cycles[1])
+
+	cycles, err = cycleRepo.FindByProblemSetID(ctx, set.ID, true)
+	assert.NoError(err)
+	assert.Len(cycles, 1)
+	assert.Equal(cycle2, cycles[0])
+
+	cycles, err = cycleRepo.FindByProblemSetID(ctx, "missing-id", false)
+	assert.NoError(err)
+	assert.NotNil(cycles)
+	assert.Empty(cycles)
+
+	ctx, cancel := context.WithCancel(ctx)
+	cancel()
+	_, err = cycleRepo.FindByProblemSetID(ctx, set.ID, false)
+	assert.True(errors.Is(err, context.Canceled))
+}
