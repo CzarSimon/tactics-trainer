a9bb382a8b3dbac7a18eebcdf70fa9aa7799ec54
Simon Lindgren <simon.g.lindgren@gmail.com>
2021-11-12 02:54:21 +0100

    puzzle-server: added cycle migrations mysql!

---
+++ puzzle-server/resources/db/mysql/3__cycles.sql
@@ -0,0 +1,16 @@
+-- +migrate Up
+CREATE TABLE `cycle` (
+    `id` VARCHAR(36) NOT NULL,
+    `number` INTEGER NOT NULL,
+    `problem_set_id` VARCHAR(36) NOT NULL,
+    `current_puzzle_id` VARCHAR(36) NOT NULL,
+    `compleated_at` DATETIME,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`problem_set_id`) REFERENCES `problem_set` (`id`),
+    FOREIGN KEY (`current_puzzle_id`) REFERENCES `puzzle` (`id`),
+    UNIQUE(`problem_set_id`, `number`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+-- +migrate Down
+DROP TABLE IF EXISTS `cycle`;
