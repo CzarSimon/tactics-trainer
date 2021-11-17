8b3893c4e911a3a23b1f6cb28560688b11b5796a
Simon Lindgren <simon.g.lindgren@gmail.com>
2021-11-17 23:10:22 +0100

    acl-updater: added initial version

---
+++ acl-updater/go.mod
@@ -0,0 +1,25 @@
+module github.com/CzarSimon/tactics-trainer/acl-updater
+
+go 1.17
+
+require (
+	github.com/CzarSimon/httputil v0.0.0-20211025070343-88cb3d2b3307
+	github.com/scaleway/scaleway-sdk-go v1.0.0-beta.7
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
+	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ acl-updater/main.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/CzarSimon/httputil/environ"
+	"github.com/CzarSimon/httputil/logger"
+	"github.com/scaleway/scaleway-sdk-go/api/k8s/v1"
+	"github.com/scaleway/scaleway-sdk-go/api/rdb/v1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+var log = logger.GetDefaultLogger("acl-updater/main").Sugar()
+
+type config struct {
+	region     scw.Region
+	clusterID  string
+	instanceID string
+	aclIgnore  string
+}
+
+type aclUpdate struct {
+	toAdd    []*rdb.ACLRuleRequest
+	toRemove []string
+}
+
+func main() {
+	c := getClient()
+	cfg := getConfig()
+	update := getAclUpdate(c, cfg)
+	updateACLRules(c, cfg, update)
+}
+
+func getClient() *scw.Client {
+	accessKey := environ.MustGet("SCW_ACCESS_KEY")
+	secretKey := environ.MustGet("SCW_SECRET_KEY")
+	c, err := scw.NewClient(scw.WithAuth(accessKey, secretKey))
+	if err != nil {
+		log.Fatal("failed to initalize client. Error: %w", err)
+	}
+
+	return c
+}
+
+func getAclUpdate(c *scw.Client, cfg config) aclUpdate {
+	nodeCIDRs := listNodeCIDRs(c, cfg)
+	ruleCIDRs := listACLRuleCIDRs(c, cfg)
+
+	update := aclUpdate{
+		toAdd:    make([]*rdb.ACLRuleRequest, 0),
+		toRemove: make([]string, 0),
+	}
+	for cidr, n := range nodeCIDRs {
+		_, ok := ruleCIDRs[cidr]
+		if ok {
+			continue
+		}
+
+		newACL := newACLRequest(cidr, n)
+		update.toAdd = append(update.toAdd, newACL)
+	}
+
+	for cidr, r := range ruleCIDRs {
+		_, ok := nodeCIDRs[cidr]
+		if ok || r.Description == cfg.aclIgnore {
+			continue
+		}
+
+		update.toRemove = append(update.toRemove, cidr)
+	}
+
+	return update
+}
+
+func newACLRequest(cidr string, node *k8s.Node) *rdb.ACLRuleRequest {
+	_, ipnet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		log.Fatal("failed to parse cidr %s. Error: %w", cidr, err)
+	}
+
+	return &rdb.ACLRuleRequest{
+		IP: scw.IPNet{
+			IPNet: *ipnet,
+		},
+		Description: fmt.Sprintf("allow ingress for node/%s", node.ID),
+	}
+}
+
+func listNodeCIDRs(c *scw.Client, cfg config) map[string]*k8s.Node {
+	req := &k8s.ListNodesRequest{
+		Region:    cfg.region,
+		ClusterID: cfg.clusterID,
+	}
+
+	res, err := k8s.NewAPI(c).ListNodes(req)
+	if err != nil {
+		log.Fatal("failed to list k8s nodes for %s. Error: %w", cfg, err)
+	}
+
+	return mapNodeCIDRs(res.Nodes)
+}
+
+func mapNodeCIDRs(nodes []*k8s.Node) map[string]*k8s.Node {
+	cidrs := make(map[string]*k8s.Node)
+	for _, n := range nodes {
+		cidr := fmt.Sprintf("%s/32", n.PublicIPV4.String())
+		cidrs[cidr] = n
+	}
+
+	return cidrs
+}
+
+func listACLRuleCIDRs(c *scw.Client, cfg config) map[string]*rdb.ACLRule {
+	req := &rdb.ListInstanceACLRulesRequest{
+		Region:     cfg.region,
+		InstanceID: cfg.instanceID,
+	}
+
+	res, err := rdb.NewAPI(c).ListInstanceACLRules(req)
+	if err != nil {
+		log.Fatal("failed to list database ACLs for %s. Error: %w", cfg, err)
+	}
+
+	return mapAclCIDRs(res.Rules)
+}
+
+func mapAclCIDRs(rules []*rdb.ACLRule) map[string]*rdb.ACLRule {
+	cidrs := make(map[string]*rdb.ACLRule)
+	for _, r := range rules {
+		cidrs[r.IP.String()] = r
+	}
+
+	return cidrs
+}
+
+func updateACLRules(c *scw.Client, cfg config, update aclUpdate) {
+	api := rdb.NewAPI(c)
+	addACLRules(api, cfg, update.toAdd)
+	time.Sleep(time.Second)
+	deleteACLRules(api, cfg, update.toRemove)
+}
+
+func deleteACLRules(api *rdb.API, cfg config, toRemove []string) {
+	if len(toRemove) < 1 {
+		return
+	}
+
+	deleteRequest := &rdb.DeleteInstanceACLRulesRequest{
+		Region:     cfg.region,
+		InstanceID: cfg.instanceID,
+		ACLRuleIPs: toRemove,
+	}
+	_, err := api.DeleteInstanceACLRules(deleteRequest)
+	if err != nil {
+		log.Fatal("failed to delete ACLs for %v. Error: %w", toRemove, err)
+	}
+}
+
+func addACLRules(api *rdb.API, cfg config, toAdd []*rdb.ACLRuleRequest) {
+	if len(toAdd) < 1 {
+		return
+	}
+
+	addRequest := &rdb.AddInstanceACLRulesRequest{
+		Region:     cfg.region,
+		InstanceID: cfg.instanceID,
+		Rules:      toAdd,
+	}
+	_, err := api.AddInstanceACLRules(addRequest)
+	if err != nil {
+		log.Fatal("failed to add ACLs for %v. Error: %w", toAdd, err)
+	}
+}
+
+func getConfig() config {
+	regionStr := environ.MustGet("SCALEWAY_REGION")
+	region, err := scw.ParseRegion(regionStr)
+	if err != nil {
+		log.Fatal("failed to parse region %s. Error: %w", regionStr, err)
+	}
+
+	return config{
+		region:     region,
+		clusterID:  environ.MustGet("K8S_CLUSTER_ID"),
+		instanceID: environ.MustGet("RDB_INSTANCE_ID"),
+		aclIgnore:  environ.Get("ACL_IGNORE", ""),
+	}
+}
+
+func (cfg config) String() string {
+	return fmt.Sprintf(
+		"config(region=%s, clusterId=%s, instanceId=%s)",
+		cfg.region,
+		cfg.clusterID,
+		cfg.instanceID,
+	)
+}
