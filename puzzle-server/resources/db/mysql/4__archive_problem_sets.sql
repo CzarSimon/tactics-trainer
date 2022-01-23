bb19742298f7f616e14b249351527c78dae31bbe
Simon Lindgren <simon.g.lindgren@gmail.com>
2022-01-23 22:25:57 +0100

    puzzle-server: correcte mysql database migration

--- puzzle-server/resources/db/mysql/4__archive_problem_sets.sql
+++ puzzle-server/resources/db/mysql/4__archive_problem_sets.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 ALTER TABLE `problem_set`
-ADD COLUMN `archived` BOOLEAN DEFAULT 'FALSE' NOT NULL
+ADD COLUMN `archived` BOOLEAN NOT NULL DEFAULT 0
 AFTER `user_id`;
 -- +migrate Down
 ALTER TABLE `problem_set` DROP COLUMN `archived`;
